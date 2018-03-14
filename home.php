cce7448e93d7ac08033e03b932cf046af8045f94
jui789 <sabrinaferdous95@gmail.com>
2018-03-14 21:33:22 +0600

    updated code

--- home.php
+++ home.php
@@ -37,6 +37,62 @@ $connection = new mysqli($servername, $username, $password, $dbname);
 
 <div class="container">
 
+
+<div class="row">
+	<form class="form-horizontal" action="search.php" method="POST">
+	<div class="form-group">
+		<div class="row">
+			<div class="col-sm-offset-4 col-sm-4">
+				<input type="text" class="form-control" name="data">
+			</div>
+			<div class="col-sm-4">
+	      		<button type="submit" class="btn btn-primary">Search</button>
+	    	</div>
+	</div>
+	</div>
+	
+
+    
+</form>
+</div>
+
+
+ <div class="row">
+ 
+                <form class="form-horizontal" action="csv_export_import.php" method="post" name="upload_excel" enctype="multipart/form-data">
+ 
+                        <!-- File Button -->
+                        <div class="form-group">
+                        	<div class="row">
+                            <label class="col-md-4 control-label" for="filebutton">Select CSV file to upload</label>
+                            <div class="col-md-4">
+                                <input type="file" name="file" id="file" class="input-large">
+                            </div>
+                        </div>
+ 
+                        <!-- Button -->
+                        <div class="form-group">
+                            <div class="col-md-4 col-md-offset-4">
+                                <button type="submit" id="submit" name="Import" class="btn btn-primary button-loading" data-loading-text="Loading...">Import</button>
+                            </div>
+                        </div>
+                </form>
+ 
+            </div>
+
+
+ 		<div class="row">
+            <form class="form-horizontal" action="csv_export_import.php" method="post" name="upload_excel"   
+                      enctype="multipart/form-data">
+                  <div class="form-group">
+                            <div class="col-md-4 col-md-offset-4">
+                                <input type="submit" name="Export" class="btn btn-success" value="export to excel"/>
+                            </div>
+                   </div>                    
+            </form>           
+ 		</div>
+
+
  <table class="table table-striped">
  	<thead>
  		<th>Full Name</th>
